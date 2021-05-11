f9093267b2ec9ff046a9273cc96b4c0dd766b990
十百千 <x1001000@users.noreply.github.com>
2021-05-12 02:27:15 +0800

    Update and rename backup_update_log.sh to crontab_notify.sh

--- crontab_notify.sh
+++ crontab_notify.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
- 
-now="$(date +'%Y%m%d')"
-cp -u /home/pi/log/"$now"* /media/pi/*/
-
-cd /home/pi/Scan/ && git pull
 
 url='https://script.google.com/macros/s/AKfycbx1hU4T2L4_nmlKJp-NENxifkcJhKJk7Fe0uWApZi3ZrIsdUq2ROc1xT7-PVXZO2-iH/exec'
 mac=$(ifconfig wlan0 | grep ether| awk {'print $2'} | sed -r 's/://g')
