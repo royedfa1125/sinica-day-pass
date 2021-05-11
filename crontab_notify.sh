02f2dab2031d1af2ae405b531d435938637e4ee8
YunJ <royedfa5229@hotmail.com>
2021-05-12 03:39:29 +0800

    update url of deployed web app

--- crontab_notify.sh
+++ crontab_notify.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-url='https://script.google.com/macros/s/AKfycbx1hU4T2L4_nmlKJp-NENxifkcJhKJk7Fe0uWApZi3ZrIsdUq2ROc1xT7-PVXZO2-iH/exec'
+url='https://script.google.com/macros/s/AKfycbw7kzX-9vdikkEBVQIPiFqUQFj00q_dGDGetQWeDGpaJK9sxPUMQWOQQq1fD_DnVAg3/exec'
 mac=$(sudo ifconfig wlan0 | grep ether| awk {'print $2'} | sed -r 's/://g')
 commit=$(cd /home/pi/Scan && git log -1 | tail -n 1| sed -r 's/\ /_/g')
 url_final=$url"?id="$mac"&commit="$commit
