d186b22ca61583755d0311ba8a449cba976eb6e4
YunJ <royedfa5229@hotmail.com>
2021-05-11 23:46:05 +0800

    for crontab add commit

--- backup_update_log.sh
+++ backup_update_log.sh
@@ -5,7 +5,8 @@ cp -u /home/pi/log/"$now"* /media/pi/*/
 
 cd /home/pi/Scan/ && git pull
 
-mac=$(ifconfig | grep ether| awk {'print $2'} | sed -r 's/://g')
-url="https://script.google.com/macros/s/AKfycbzuze4yIhfZF02vs_rmmtICdNMr1IYEOjpMBWuSq8-ax1yth6Mrr3cR/exec?id="
-url_final=$url$mac
+url='https://script.google.com/macros/s/AKfycbx1hU4T2L4_nmlKJp-NENxifkcJhKJk7Fe0uWApZi3ZrIsdUq2ROc1xT7-PVXZO2-iH/exec'
+mac=$(ifconfig wlan0 | grep ether| awk {'print $2'} | sed -r 's/://g')
+commit=$(git log -1 | tail -n 1| sed -r 's/\ /_/g')
+url_final=$url"?id="$mac"&commit="$commit
 curl $url_final
