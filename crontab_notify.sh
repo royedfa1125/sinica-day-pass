449dfc4c4005714cf59c96d930bb31c3f167ad91
十百千 <x1001000@users.noreply.github.com>
2021-05-12 03:21:13 +0800

    only mac address needs sudo

--- crontab_notify.sh
+++ crontab_notify.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 url='https://script.google.com/macros/s/AKfycbx1hU4T2L4_nmlKJp-NENxifkcJhKJk7Fe0uWApZi3ZrIsdUq2ROc1xT7-PVXZO2-iH/exec'
-mac=$(ifconfig wlan0 | grep ether| awk {'print $2'} | sed -r 's/://g')
-commit=$(git log -1 | tail -n 1| sed -r 's/\ /_/g')
+mac=$(sudo ifconfig wlan0 | grep ether| awk {'print $2'} | sed -r 's/://g')
+commit=$(cd /home/pi/Scan && git log -1 | tail -n 1| sed -r 's/\ /_/g')
 url_final=$url"?id="$mac"&commit="$commit
 curl $url_final
